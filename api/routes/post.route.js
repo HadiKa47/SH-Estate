5dac3ea2cb24287f133fc9567643b49fd0a3f9fc
HadiKa47 <Hade.Kary24@gmail.com>
2024-06-30 10:08:44 +0300

    Complete Register api and Connect to Prisma and MongoDB

---
+++ api/routes/post.route.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+const router = express.Router();
+
+// router.get("/test", (req, res) => {
+//   console.log("router working");
+// });
+// router.post("/test", (req, res) => {
+//   console.log("router working");
+// });
+// router.put("/test", (req, res) => {
+//   console.log("router working");
+// });
+// router.delete("/test", (req, res) => {
+//   console.log("router working");
+// });
+
+export default router;
